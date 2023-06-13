dff342a08f3d4d218a50b6e9b9c3312293bd7a63
Alison Matos <alisonrafaeldematos@gmail.com>
2023-06-13 15:40:53 -0300

    first commit

---
+++ projeto_cad_usuario/projeto_cad_usuario/urls.py
@@ -0,0 +1,7 @@
+from app_cadastrando import views 
+from django.urls import path
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('usuarios/',views.usuarios,name='listagem_usuarios')
+]
